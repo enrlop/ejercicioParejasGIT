62f7bd18d37df39eeec70aeaddd645483f8d9082
Enrique <enrique.lopapa@educa.jcyl.es>
2024-04-26 18:51:07 +0200

    añadiendo codigo

--- index.php
+++ index.php
@@ -5,3 +5,10 @@
 
     hola();
 ?>
+
+<?php
+$nombre = "Maria";
+echo "¡Hola! Mi nombre es " . $nombre . "<br>";
+echo "¡Hola! Mi nombre es " . $NOMBRE . "<br>";
+echo "¡Hola! Mi nombre es " . $NomBre . "<br>";
+?>
